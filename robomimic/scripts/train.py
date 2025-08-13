2a6d2626317722cee68b7821bdfd6bf1752af1c5
hjl1013 <hjl1013@snu.ac.kr>
2025-08-13 13:54:04 +0900

    modified debug path

--- robomimic/scripts/train.py
+++ robomimic/scripts/train.py
@@ -476,7 +476,7 @@ def main(args):
         config.experiment.rollout.horizon = 10
 
         # send output to a temporary directory
-        config.train.output_dir = "/tmp/tmp_trained_models"
+        config.train.output_dir = "/home/hyunjun/projects/p-self-improvement/training/debug/PnPCounterToCab_DP/PnPCounterToCab_DP"
 
     # lock config to prevent further modifications and ensure missing keys raise errors
     config.lock()
